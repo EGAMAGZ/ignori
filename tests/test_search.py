426c40ce3b76ac2c3ed10116847268911cce1697
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-07-19 15:08:14 -0600

    chore: Installed pytest and pytest-asyncio inside "test" dependencies group

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,2 +1,11 @@
-def test_empty_search() -> None:
+import pytest
+
+from ignori.app import IgnoriApp
+
+
+@pytest.mark.asyncio
+async def test_empty_search() -> None:
+    app = IgnoriApp()
+    async with app.run_test() as pilot:
+        await pilot.click("#search-button")
     assert 2 + 3 == 5
